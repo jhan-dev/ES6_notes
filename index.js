20059168119dc7821464fddc799926fba6048dea
jhan-dev <jhan.technology@gmail.com>
2023-04-28 13:54:13 -0700

    Async/await

--- index.js
+++ index.js
@@ -808,6 +808,26 @@ spaceNames().then(console.log);
 // Output: Chris Cassidy, Anatoly Ivanishin, Ivan Vagner, Doug Hurley, Bob Behnken
 
 //================================================================
-// 
+// Async/await returns promise and the resolve value of the promise will 
+// be what ever is returned from the function.
+
+const delay = (seconds) =>
+  new Promise((resolve) => 
+    setTimeout(resolves, seconds * 1000)
+  );
+
+const countToFive = async () => {
+  console.log("zero seconds");
+  await delay(1);
+  console.log("one second");
+  await delay(2);
+  console.log("two seconds");
+  await delay(3);
+  console.log("three seconds");
+};
+
+countToFive();
+
+
 
 
